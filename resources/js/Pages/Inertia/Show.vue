a985d2ccf73627c99f7377db59d9de9a8653cd71
temstech <masashi-terui@tems-tech.com>
2023-05-06 22:29:07 +0900

    inertia-vue.js

---
+++ resources/js/Pages/Inertia/Show.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { Inertia } from "@inertiajs/inertia";
+
+defineProps({
+    id: String,
+    blog: Object,
+})
+
+const deleteConform = id => {
+    // console.log(id)
+    Inertia.delete(`/inertia/${id}`, {
+        onBefore: () => confirm('本当に削除しますか？')
+    })
+}
+</script>
+
+<template>
+    {{ id }}<br>
+    {{ blog.title }}<br>
+    <button @click="deleteConform(blog.id)">削除</button>
+</template>
